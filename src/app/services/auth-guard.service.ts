ce939d3e7d22328cb410275b1963d53785d8e8b9
Rodolfo Silva <rodolfo.diego.gomes@gmail.com>
2024-07-09 19:22:43 -0400

    Fix JwtInterceptorService

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -6,25 +6,16 @@ import { Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
-  //console.log('AuthGuard: Checking authentication status');
- // console.log('Current URL:', state.url);
- // console.log('User is logged in:', authService.isLoggedIn());
-
   if (authService.isLoggedIn()) {
     if (state.url === '/login' || state.url === '/register') {
-   //   console.log('User is already logged in, redirecting to /monitorings');
       router.navigate(['/monitorings']);
       return false;
     }
-  //  console.log('User is logged in, allowing access');
     return true;
   } else {
     if (state.url === '/login' || state.url === '/register') {
-   //   console.log('User is not logged in, allowing access to login/register');
       return true;
     }
-   // console.log('User is not logged in, redirecting to /login');
     router.navigate(['/login']);
     return false;
   }
